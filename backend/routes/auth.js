c282d5d52d8368e05d96b6b767afa63d59a26a88
Nishant <pnishant0824@gmail.com>
2024-12-14 00:57:32 +0530

    sign up complete

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,21 @@
 const router = require("express").Router();
 const User = require("../models/user");
+const bcrypt =require("bcrypt");
+
+
+//SIGN UP
 
 router.post("/register", async (req, res) => {
     try {
         const { email, username, password } = req.body;
-
+        
         // Validate required fields
         if (!email || !username || !password) {
             return res.status(400).json({ message: "All fields are required" });
         }
+        
+        const saltRounds=10;
+        const hashpassword=await bcrypt.hashSync(password,saltRounds);
 
         // Check if the user already exists
         const existingUser = await User.findOne({ email });
@@ -17,7 +24,7 @@ router.post("/register", async (req, res) => {
         }
 
         // Create a new user
-        const user = new User({ email, username, password });
+        const user = new User({ email, username, password:hashpassword });
         await user.save();
 
         res.status(200).json({ message: "User registered successfully", user });
@@ -25,5 +32,9 @@ router.post("/register", async (req, res) => {
         res.status(500).json({ message: "Server error", error });
     }
 });
+
+//LOG IN 
+
+router
 
 module.exports = router;
